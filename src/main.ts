bb17220296b7e8fb1ceed4bc163e67ccc64f9db8
Kalina Dukova <kalina984@abv.bg>
2024-09-10 14:25:46 +0300

    add tailwind

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,8 @@ import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 
+import './assets/index.css'
+
 import App from './App.vue'
 import router from './router'
 
